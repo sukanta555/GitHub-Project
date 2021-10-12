202e1ede0e60dd4b3f4d74ce6415353f76311c47
Sukanta <sukantab566@gmail.com>
2021-10-13 00:32:30 +0530

    Log i addition

--- app/src/main/java/com/example/githubpro/MainActivity.java
+++ app/src/main/java/com/example/githubpro/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.githubpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+        Log.i( "Tag", "onCreate");
     }
 }

a070190468671219f1eb4a5447c87d019f11afb7
Sukanta <sukantab566@gmail.com>
2021-10-12 22:41:21 +0530

    Add Toast message

--- app/src/main/java/com/example/githubpro/MainActivity.java
+++ app/src/main/java/com/example/githubpro/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.githubpro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Test Git pro",Toast.LENGTH_SHORT).show();
     }
 }
